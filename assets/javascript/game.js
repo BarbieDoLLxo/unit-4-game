a9c2054a3e7b440278fe7835a88c2e10e04b77a6
barbiedoll <Barbie.Slumerican@gmail.com>
2019-07-14 22:16:12 -0600

    HW4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,86 @@
+$(document).ready(function() {
+  var total = 0;
+  var compGuess;
+  var wins = 0;
+  var losses = 0;
 
-var Wins = 0;
-var losses = 0;
-var userClick= [""];
+  var diamond;
+  var ruby;
+  var opal;
+  var emerald;
 
+  function gameStart() {
+    compGuess = Math.floor(Math.random() * 500);
 
+    diamond = Math.floor(Math.random() * 25);
+    ruby = Math.floor(Math.random() * 70);
+    opal = Math.floor(Math.random() * 10);
+    emerald = Math.floor(Math.random() * 5);
+    console.log(diamond, ruby, opal, emerald, compGuess);
+
+    $("#M").text("Match This Number " + compGuess);
+  }
+
+  gameStart();
+
+  function winner() {
+    if (compGuess === total) {
+      alert("you win");
+      wins = wins + 1;
+      total = 0;
+      $("#W").text("Wins " + wins);
+      gameStart();
+    }
+  }
+
+  function lose() {
+    if (total > compGuess) {
+      alert("you lose");
+      losses = losses + 1;
+      total = 0;
+      $("#L").text("Losses " + losses);
+      gameStart();
+      console.log(losses);
+    }
+  }
+
+  function userTotal() {
+    $("#T").text("Your Total " + total);
+  }
+
+  $("#D").on("click", function() {
+    total = total + diamond;
+    console.log(total);
+    console.log(diamond);
+    userTotal();
+    winner();
+    lose();
+  });
+
+  $("#R").on("click", function() {
+    total = total + ruby;
+    console.log(total);
+    console.log(ruby);
+    userTotal();
+    winner();
+    lose();
+  });
+
+  $("#O").on("click", function() {
+    total = total + opal;
+    console.log(total);
+    console.log(opal);
+    userTotal();
+    winner();
+    lose();
+  });
+
+  $("#E").on("click", function() {
+    total = total + emerald;
+    console.log(total);
+    console.log(emerald);
+    userTotal();
+    winner();
+    lose();
+  });
+});
